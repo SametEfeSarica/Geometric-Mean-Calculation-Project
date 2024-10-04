3250ce6aabffb3ad4525609da579ee3ec1519186
Samet Efe Sarıca <sametefes63@gmail.com>
2024-10-05 02:23:26 +0300

    Create Geometrik Hesaplama.cs

---
+++ Geometrik Hesaplama.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometrikOrtalamaHesabıÖdevi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 2 Sayının Geometrik Ortalaması alınacak. 
+            // Küsüratlı çıkabileceği için Double veri tipini kullandım.
+            // RedLine komutu ile Double komutu çalışmayacağından komutu "Convert" yardımıyla Double'a dönüştürdüm.
+
+            Console.WriteLine("İlk Sayıyı Girin:");
+
+            Double sayi1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("İkinci Sayıyı Girin:");
+
+            Double sayi2 = Convert.ToDouble(Console.ReadLine());
+
+            Double sayi3 = Convert.ToDouble(Math.Sqrt(sayi1 * sayi2));
+
+            // Sonuç Yazılacak.
+            
+            Console.WriteLine("İşte Sonuç:");
+
+            Console.WriteLine(sayi3);
+
+            //Terminalin ekranda Kalması için.
+
+            Console.ReadLine();
+        }
+    }
+}
